4957869ccc91e59b13bb8633f6759c0c7a14bf32
ManuelGM <manuel.gonzalez.0518@gmail.com>
2021-12-09 18:23:26 -0600

    JFlap to class

--- lib/pages/phone_controller.dart
+++ lib/pages/phone_controller.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:pili_phone/models/automaton.dart';
+import 'package:xml/xml.dart';
 
 class PhoneController extends GetxController {
   final numberField = TextEditingController();
   final coinsField = TextEditingController();
+
+  @override
+  void onInit() async {
+    final test = await Automaton.fromFile('assets/machines/Proyecto 1.jff');
+    print('Type: ${test.type}');
+    print('STATES:');
+    for (final state in test.states) {
+      print(state);
+    }
+    print('TRANSITIONS:');
+    for (final transition in test.transitions) {
+      print(transition);
+    }
+
+    //print(document.toXmlString(pretty: true, indent: '\t'));
+    super.onInit();
+  }
 }
